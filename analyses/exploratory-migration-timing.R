a27fefb9e2b9d8c23edd1d26e04b5bfd02982a7f
mackwhite <mwhite@fiu.edu>
2024-07-20 00:36:05 -0400

    adding general movement plot code here

--- analyses/exploratory-migration-timing.R
+++ analyses/exploratory-migration-timing.R
@@ -12,8 +12,11 @@ dt_hydro <- left_join(dt, hydro, by = "date") |>
       mutate(date = as.Date(date),
              year = year(date),
              month = month(date),
+             yearmonth = paste(year, month, sep = "-"),
              dayofyear = yday(date))
 glimpse(dt_hydro)
+
+# looking at inter-annual timing of multi-yr fishes -----------------------
 
 dt_hydro_filter <- dt_hydro |> 
       group_by(id) |> 
@@ -25,25 +28,6 @@ unique(dt_hydro_filter$id)
 breaks <- yday(as.Date(paste0("2023-", 1:12, "-01")))
 labels <- month.name[1:12]
 
-
-dt_hydro |> 
-      # group_by(id, date, distance) |> 
-      # mutate(mean_dist = mean(distance)) |> 
-      filter(id == "A69-1303-32892") |> 
-      ggplot() +
-      geom_line(aes(x = dayofyear, y = distance, color = as.factor(year)), size = 1) +
-      scale_x_continuous(breaks = breaks, labels = labels) +
-      theme_classic() +
-      labs(title = "A69-1303-32892",
-           x = 'date',
-           y = 'distance') +
-      scale_fill_brewer(palette = "Set1") + 
-      theme(axis.text.x = element_text(angle = 45, hjust = 1),
-            panel.background = element_rect(fill = "white"),
-            axis.line = element_line("black"),
-            axis.text = element_text(face = "bold"),
-            axis.title = element_text(face = "bold"))
-
 migration_plot <- function(f) {
       dt_hydro_filter |> 
             filter(id == f) |> 
@@ -51,7 +35,7 @@ migration_plot <- function(f) {
             geom_line(aes(x = dayofyear, y = distance, color = as.factor(year)), size = 1) +
             scale_x_continuous(breaks = breaks, labels = labels) +
             theme_classic() +
-            labs(title = "A69-1303-32892",
+            labs(title = f,
                  x = 'date',
                  y = 'distance') +
             scale_fill_brewer(palette = "Set1") + 
@@ -66,8 +50,38 @@ migration_plot <- function(f) {
 migration_test <- map(unique(dt_hydro_filter$id), migration_plot)
 
 ggsave(
-      filename = "migration-timing-test.pdf",
+      filename = "inter-annual-migration-timing.pdf",
       # path = "migration_test",
       plot = marrangeGrob(migration_test, nrow = 1, ncol = 1),
+      width = 15, height = 9
+)
+
+# looking at all movement plots -------------------------------------------
+
+migration_plot2 <- function(f) {
+      dt_hydro |> 
+            filter(id == f) |> 
+            ggplot() +
+            geom_line(aes(x = date, y = distance, color = as.factor(year)), size = 1) +
+            # scale_x_continuous(breaks = breaks, labels = labels) +
+            theme_classic() +
+            labs(title = f,
+                 x = 'date',
+                 y = 'distance') +
+            scale_fill_brewer(palette = "Set1") + 
+            theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                  panel.background = element_rect(fill = "white"),
+                  axis.line = element_line("black"),
+                  axis.text = element_text(face = "bold"),
+                  axis.title = element_text(face = "bold"))
+}
+
+# Generate boxplot figures for each projecthabitat
+migration_test2 <- map(unique(dt_hydro$id), migration_plot2)
+
+ggsave(
+      filename = "general-movement-plots.pdf",
+      # path = "migration_test",
+      plot = marrangeGrob(migration_test2, nrow = 1, ncol = 1),
       width = 15, height = 9
 )
