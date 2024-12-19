b49a453375e6e7c8261f3282019202bbb8c9d48c
mackwhite <mwhite@fiu.edu>
2024-12-19 09:21:22 -0500

    appending recent download cycle to the period of record

---
+++ database management/2024_Fall/SharkRiverMovementCode_Append_Fall2024_DownloadCycle.R
@@ -0,0 +1,125 @@
+###project: SRFCEA
+###author(s): Mack White
+###goal(s): append new detections to period of record file
+###date(s): April 2024
+###note(s): stations misproperly assigned in November corrected in script titled
+# "station reassignment_sprin2024.R"
+
+###########################################################################
+# Housekeeping ------------------------------------------------------------
+###########################################################################
+
+### load necessary libraries
+### install.packages("librarian")
+librarian::shelf(tidyverse, scales, ggplot2)
+
+# define custom save_rds function -----------------------------------------
+
+save_rds <- function(data, filename) {
+      saveRDS(data, file = filename)
+      return(data)
+}
+
+# read in vue offload from previous download ------------------------------
+
+NewestVUE <- read_csv("database management/2024_Fall/Fall2024_VUEoffload.csv")
+glimpse(NewestVUE)
+
+# manipulate data to match format of previous all vue rds file ------------
+NewestVUE1 <- NewestVUE |> 
+      select(-`Transmitter Type`, -`Sensor Precision`) |> 
+      mutate(across(c(Receiver, Transmitter, `Transmitter Name`, 
+                      `Sensor Unit`, `Station Name`), factor),
+             Datetime_UTC = as.POSIXct(`Date and Time (UTC)`,
+                                            format = "%Y-%m-%d %H:%M:%S")) |> 
+      select(-`Date and Time (UTC)`) |> 
+      ### renaming to join with old vue dataset
+      ### this could be a function of using the VR2TXs (?) - but appears all data
+      ### made its way over from vue, so not worried about issues between
+      ### VR2W and VRT2X info
+      rename(
+            Transmitter.Name = `Transmitter Name`,
+            Transmitter.Serial = `Transmitter Serial`,
+            Sensor.Value = `Sensor Value`,
+            Sensor.Unit = `Sensor Unit`,
+            Station.Name = `Station Name`
+      ) |> 
+      select(Datetime_UTC, everything()) |> 
+      mutate(Transmitter.Serial = as.integer(Transmitter.Serial))
+glimpse(NewestVUE1)
+
+### Mack added this to aboce code chunk for ease
+# NewestVUE2 <- NewestVUE1[c(ncol(NewestVUE1), 1:(ncol(NewestVUE1)-1))]
+# NewestVUE2$Transmitter.Serial <- as.integer(NewestVUE2$Transmitter.Serial)
+# glimpse(NewestVUE2)
+
+# read in "old" All VUE file generated during previous append -------------
+OldVUE <- readRDS("database management/2024_Fall/FinalVUE_wDatetime_04222024_ALL.rds")
+glimpse(OldVUE)
+
+# make sure everything matches up for rbind -------------------------------
+glimpse(OldVUE)
+glimpse(NewestVUE2)
+
+AllVUE <- rbind(OldVUE, NewestVUE1)
+### manually sum old and new number of observations to ensure everything 'adds' up haha
+# 42693219+1706838
+
+AllVUE$Datetime_UTC <- as.POSIXct(AllVUE$Datetime_UTC, 
+                                  format="%Y-%m-%d %H:%M:%S", tz = "UTC")
+glimpse(AllVUE)
+
+### save this vue dataframe - it will becomes OldVUE on next append to period of record
+# saveRDS(AllVUE, "database management/2024_Fall/RDS_files/FinalVUE_wDatetime_12192024_ALL.rds")
+
+rm(list = ls()) #clear env to save memory
+FinalVUE <- readRDS("database management/2024_Fall/RDS_files/FinalVUE_wDatetime_12192024_ALL.rds")
+unique(FinalVUE$Station.Name)
+FinalVUE2 <- readRDS("database management/2024_Spring/RDS_files/FinalVUE_wDatetime_04222024_ALL.rds")
+unique(FinalVUE2$Station.Name)
+vue_check <- FinalVUE |> 
+      mutate(
+            Year = year(Datetime_UTC),
+            Month = month(Datetime_UTC),
+            YearMonth = paste(Year,Month, sep = "-")
+      )
+
+vue_check <- as.data.frame(unique(vue_check$YearMonth))
+rm(vue_check)
+
+# read in tag list and station data ---------------------------------------
+TagList <- read_csv("database management/core_data/Acoustic Tags Master List_06132023update.csv") |> 
+      mutate(Transmitter = as.factor(Transmitter))
+StationData <- read_csv("database management/core_data/Station_Distance_Updated07142020.csv") |> 
+      mutate(Station.Name = as.factor(VUE_Name))
+
+# join station, tag, and detection data -----------------------------------
+RawVUE <- FinalVUE |> 
+      select(-Latitude, -Longitude)
+RehageVUE <- left_join(RawVUE, TagList, by = "Transmitter")
+RehageVUE_Distance <- left_join(RehageVUE, StationData, by = "Station.Name") |> 
+      distinct()
+
+### keeps all detections on record, but could sort for our tags/make assumptions about identity of others (cough cough bull sharks) if wanted for some other analyses 
+saveRDS(RehageVUE_Distance, "database management/2024_Fall/RDS_files/RehageVUE_All_12192024.rds")
+
+HeithausVUE_Distance <- RehageVUE_Distance |> filter(is.na(Species))
+saveRDS(HeithausVUE_Distance, "database management/2024_Fall/RDS_files/HeithausVUE_All_12192024_wDISTANCE.rds")
+
+# generate period of record files for species -----------------------------
+
+RehageVUE_Distance |> 
+      filter(Species == "Snook") |> 
+      save_rds("database management/2024_Fall/RDS_files/AllSnook_POR_12192024.rds")
+
+RehageVUE_Distance |> 
+      filter(Species == "Bass") |> 
+      save_rds("database management/2024_Fall/RDS_files/AllBass_POR_12192024.rds")
+
+RehageVUE_Distance |> 
+      filter(Species == "Redfish") |> 
+      save_rds("database management/2024_Fall/RDS_files/AllRedfish_POR_12192024.rds")
+
+RehageVUE_Distance |> 
+      filter(Species == "Tarpon") |> 
+      save_rds("database management/2024_Fall/RDS_files/AllTarpon_POR_12192024.rds")
