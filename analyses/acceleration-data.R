8420a72ee887cce273f2f9f2e0f71ba2be6c4374
mackwhite <mwhite@fiu.edu>
2025-01-07 14:00:55 -0500

    adding some code to begin exploring acceleration data

--- analyses/acceleration-data.R
+++ analyses/acceleration-data.R
@@ -13,6 +13,16 @@ accelerometer <- all |> filter(tag == "accelerometer")
 unique(accelerometer$Transmitter)
 
 accelerometer |> 
-      ggplot(aes(x = as.factor(id), y = Sensor.Value)) +
-      geom_boxplot() +
-      facet_wrap(~species)
+      ggplot(aes(x = as.factor(id), y = Sensor.Value, colour = species)) +
+      geom_boxplot()
+
+### next steps are to separate the dates and time (keep datetime as well) + 
+### add temperature and hydrology data + relative prey abundance + 
+### relative snook and bass abundance + combined mesopredator abundance + 
+### moon phase/meteorological data (e.g., cloud cover?)
+### check these goodies for acceleration studies for treating data, questions, etc:
+### would maybe be interesting to see if behaviors similar to some terrestrial predators?
+
+# Kadar, Julianna, Monique Ladds, Johann Mourier, Joanna Day, and Culum Brown. 2019. “Acoustic Accelerometry Reveals Diel Activity Patterns in Premigratory Port Jackson Sharks.” Ecology and Evolution 9 (16): 8933–44.
+# Lennox, Robert J., Sindre H. Eldøy, Lotte S. Dahlmo, Jordan K. Matley, and Knut Wiik Vollset. 2023. “Acoustic Accelerometer Transmitters and Their Growing Relevance to Aquatic Science.” Movement Ecology 11 (1): 45.
+# Meese, Emily N., and Christopher G. Lowe. 2020. “Active Acoustic Telemetry Tracking and Tri-Axial Accelerometers Reveal Fine-Scale Movement Strategies of a Non-Obligate Ram Ventilator.” Movement Ecology 8 (1): 8.
